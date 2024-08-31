5c9679b66051eefbbfb09fb0ba99f462b8eb12e3
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-08-31 21:21:30 +0530

    updating hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@ import botImg from "../../public/images/bot.png"
 export default function Home() {
   return (
     <div className="homepage flex items-center h-screen gap-28 px-20">
-    
       <div className="left flex-1 flex-col items-center justify-center gap-4 text-center">
         <h1 className="text-[90px] bg-gradient-to-r from-[#217bfe] to-[#00fe40] bg-clip-text text-transparent">
           AI GIMMIK
         </h1>
-        <h2 className="">Supercharge your creativity and productivity</h2>
-        <h3 className="font-bold">
+        <h2 className="font-bold text-xl mb-5">
+          Supercharge your creativity and productivity
+        </h2>
+        <h3 className="">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
           perspiciatis ipsam aperiam voluptas aut, quas possimus neque accusamus
         </h3>
@@ -20,9 +21,9 @@ export default function Home() {
       <div className="right flex-1 flex items-center justify-center h-full">
         <div className="imgContainer flex items-center justify-center rounded-xl bg-[#140e2d] w-4/5 height-1/2">
           <div className="bgContainer">
-              <div className="bg"></div>
+            <div className="bg"></div>
           </div>
-          <Image src={botImg} alt="" width={400} height={500} className="bot"/>
+          <Image src={botImg} alt="" width={400} height={500} className="bot" />
         </div>
       </div>
     </div>
